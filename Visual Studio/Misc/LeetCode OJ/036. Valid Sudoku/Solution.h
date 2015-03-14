567554c2febb8fd1759fd447e5e0c80bf1f3b704
EFanZh <efanzh@gmail.com>
2015-03-14 14:48:29 +0800

    LeetCode OJ algorithm problem 36 is done

--- Visual Studio/Misc/LeetCode OJ/036. Valid Sudoku/Solution.h
+++ Visual Studio/Misc/LeetCode OJ/036. Valid Sudoku/Solution.h
@@ -5,5 +5,37 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>> &board)
     {
+        vector<pair<size_t, size_t>> blanks;
+        array<array<bool, 9>, 9> rows = {};
+        array<array<bool, 9>, 9> columns = {};
+        array<array<bool, 9>, 9> blocks = {};
+
+        for (size_t i = 0; i < 9; ++i)
+        {
+            for (size_t j = 0; j < 9; ++j)
+            {
+                if (board[i][j] == '.')
+                {
+                    blanks.emplace_back(i, j);
+                }
+                else
+                {
+                    if (rows[i][board[i][j] - '1'] ||
+                        columns[j][board[i][j] - '1'] ||
+                        blocks[i / 3 * 3 + j / 3][board[i][j] - '1'])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        rows[i][board[i][j] - '1'] = true;
+                        columns[j][board[i][j] - '1'] = true;
+                        blocks[i / 3 * 3 + j / 3][board[i][j] - '1'] = true;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 };
--- Visual Studio/Misc/LeetCode OJ/036. Valid Sudoku/main.cpp
+++ Visual Studio/Misc/LeetCode OJ/036. Valid Sudoku/main.cpp
@@ -4,4 +4,14 @@
 
 int main()
 {
+    array<const char *, 9> testCases[] = { { "....4.9..", "..21..3..", ".........", "........3", "...2.....", ".....7...", "...61....", "..9......", ".......9." },
+                                           { "..4...63.", ".........", "5......9.", "...56....", "4.3.....1", "...7.....", "...5.....", ".........", "........." } };
+    Solution s;
+
+    for (const auto &testCase : testCases)
+    {
+        auto board = convertStringToMatrix(testCase);
+
+        s.isValidSudoku(board);
+    }
 }
