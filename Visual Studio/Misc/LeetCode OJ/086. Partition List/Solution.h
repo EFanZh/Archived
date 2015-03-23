bb19c9fd3fa16e21eb8a014d505e02537f2200a0
EFanZh <efanzh@gmail.com>
2015-03-23 10:55:43 +0800

    LeetCode OJ algorithm problem OJ 86 is done

--- Visual Studio/Misc/LeetCode OJ/086. Partition List/Solution.h
+++ Visual Studio/Misc/LeetCode OJ/086. Partition List/Solution.h
@@ -1,17 +1,42 @@
 #pragma once
 
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution
 {
 public:
     ListNode *partition(ListNode *head, int x)
     {
+        ListNode *head1 = nullptr;
+        ListNode **tail1 = &head1;
+        ListNode *head2 = nullptr;
+        ListNode **tail2 = &head2;
+
+        while (head != nullptr)
+        {
+            if (head->val < x)
+            {
+                *tail1 = head;
+                tail1 = &head->next;
+            }
+            else
+            {
+                *tail2 = head;
+                tail2 = &head->next;
+            }
+
+            head = head->next;
+        }
+
+        *tail1 = head2;
+        *tail2 = nullptr;
+
+        return head1;
     }
 };
--- Visual Studio/Misc/LeetCode OJ/086. Partition List/main.cpp
+++ Visual Studio/Misc/LeetCode OJ/086. Partition List/main.cpp
@@ -4,4 +4,13 @@
 
 int main()
 {
+    pair<initializer_list<int>, int> testCases[] = { { { 2, 1 }, 2 } };
+    Solution s;
+
+    for (auto testCase : testCases)
+    {
+        vector<unique_ptr<ListNode>> pool;
+
+        s.partition(MakeList(pool, testCase.first), testCase.second);
+    }
 }
