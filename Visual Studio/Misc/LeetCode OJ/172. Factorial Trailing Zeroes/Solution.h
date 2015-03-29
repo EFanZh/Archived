c377c6f9d40c9f65ed66a169a003365fa9d47332
EFanZh <efanzh@gmail.com>
2015-03-29 15:40:52 +0800

    LeetCode OJ algorithm problem 172 is done

--- Visual Studio/Misc/LeetCode OJ/172. Factorial Trailing Zeroes/Solution.h
+++ Visual Studio/Misc/LeetCode OJ/172. Factorial Trailing Zeroes/Solution.h
@@ -1,9 +1,19 @@
 #pragma once
 
+// https://oeis.org/A027868
 class Solution
 {
 public:
     int trailingZeroes(int n)
     {
+        int result = 0;
+
+        while (n > 0)
+        {
+            n /= 5;
+            result += n;
+        }
+
+        return result;
     }
 };
