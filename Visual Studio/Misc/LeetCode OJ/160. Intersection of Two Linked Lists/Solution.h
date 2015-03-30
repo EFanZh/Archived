754fc1e1a2aa461ed7d466b3697771837666c1c6
EFanZh <efanzh@gmail.com>
2015-03-30 13:23:54 +0800

    LeetCode OJ algorithm problem 160 is done

--- Visual Studio/Misc/LeetCode OJ/160. Intersection of Two Linked Lists/Solution.h
+++ Visual Studio/Misc/LeetCode OJ/160. Intersection of Two Linked Lists/Solution.h
@@ -10,8 +10,46 @@
  */
 class Solution
 {
+    size_t getListLength(ListNode *list)
+    {
+        size_t result = 0;
+
+        while (list != nullptr)
+        {
+            ++result;
+            list = list->next;
+        }
+
+        return result;
+    }
+
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
     {
+        size_t l1 = getListLength(headA);
+        size_t l2 = getListLength(headB);
+
+        if (l1 < l2)
+        {
+            for (size_t i = 0; i < l2 - l1; ++i)
+            {
+                headB = headB->next;
+            }
+        }
+        else
+        {
+            for (size_t i = 0; i < l1 - l2; ++i)
+            {
+                headA = headA->next;
+            }
+        }
+
+        while (headA != headB)
+        {
+            headA = headA->next;
+            headB = headB->next;
+        }
+
+        return headA;
     }
 };
