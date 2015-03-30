90b429c372630d3da1ccc2b4604aebc1645f5e28
EFanZh <efanzh@gmail.com>
2015-03-30 09:37:58 +0800

    Prepare to do LeetCode OJ algorithm 160

---
+++ Visual Studio/Misc/LeetCode OJ/160. Intersection of Two Linked Lists/Solution.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+    }
+};
---
+++ Visual Studio/Misc/LeetCode OJ/160. Intersection of Two Linked Lists/main.cpp
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+#include "Solution.h"
+
+int main()
+{
+}
