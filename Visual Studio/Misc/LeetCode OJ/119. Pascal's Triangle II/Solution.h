5be205bd65151b1c1e7caaaea05d29330da45073
EFanZh <efanzh@gmail.com>
2015-04-10 21:30:39 +0800

    LeetCode OJ algorithm problem 119 is done

---
+++ Visual Studio/Misc/LeetCode OJ/119. Pascal's Triangle II/Solution.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Solution
+{
+public:
+    vector<int> getRow(int rowIndex)
+    {
+        ++rowIndex;
+
+        vector<int> result(rowIndex);
+        size_t middle = (rowIndex + 1) / 2;
+
+        result.front() = 1;
+
+        for (size_t i = 1; i < middle; ++i)
+        {
+            result[i] = result[i - 1] * (rowIndex - i) / i;
+        }
+
+        copy(result.cbegin(), result.cbegin() + rowIndex / 2, result.rbegin());
+
+        return result;
+    }
+};
---
+++ Visual Studio/Misc/LeetCode OJ/119. Pascal's Triangle II/main.cpp
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/pascals-triangle-ii/
+
+#include "Solution.h"
+
+int main()
+{
+}
