d34fa5f838380a105a7be8ceab046630b0a8fdd9
EFanZh <efanzh@gmail.com>
2015-04-15 17:19:20 -0700

    New search file application

---
+++ Visual Studio/Applications/Search Files/Search Files/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace SearchFiles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool shouldStop = false;
+        private Task currentTask = Task.FromResult(0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Model = new Model();
+
+            Model.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "Folder" || e.PropertyName == "Keyword")
+                {
+                    NewSearch();
+                }
+            };
+        }
+
+        public Model Model
+        {
+            get;
+            set;
+        }
+
+        private async void NewSearch()
+        {
+            shouldStop = true;
+            await currentTask;
+
+            string folder = Model.Folder;
+            Func<string, bool> predicate = s => Path.GetFileName(s).Contains(Model.Keyword);
+            var progress = new Progress<string>();
+
+            progress.ProgressChanged += (sender, e) => Model.Result.Add(e);
+
+            Model.Result.Clear();
+
+            shouldStop = false;
+
+            Model.State = "Searching…";
+
+            currentTask = Task.Run(() =>
+            {
+                Dfs(folder, predicate, progress);
+            });
+
+            await currentTask;
+            Model.State = "Done.";
+        }
+
+        private void Dfs(string folder, Func<string, bool> predicate, IProgress<string> progress)
+        {
+            var s = new Stack<string>();
+
+            s.Push(folder);
+
+            while (s.Count > 0)
+            {
+                var current = s.Pop();
+
+                if (predicate(current))
+                {
+                    progress.Report(current);
+                }
+
+                try
+                {
+                    foreach (var file in Directory.GetFiles(current).Where(predicate))
+                    {
+                        progress.Report(file);
+
+                        if (shouldStop)
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                }
+
+                try
+                {
+                    foreach (var directory in Directory.GetDirectories(current).Reverse())
+                    {
+                        s.Push(directory);
+
+                        if (shouldStop)
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                }
+
+                if (shouldStop)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Visual Studio/Applications/Search Files/Search Files/Model.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SearchFiles
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string folder = string.Empty;
+        private string keyword = string.Empty;
+        private string state = string.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Model()
+        {
+            Result = new ObservableCollection<string>();
+        }
+
+        public string Folder
+        {
+            get
+            {
+                return folder;
+            }
+            set
+            {
+                folder = value;
+
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Keyword
+        {
+            get
+            {
+                return keyword;
+            }
+            set
+            {
+                keyword = value;
+
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Result
+        {
+            get;
+            set;
+        }
+
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
