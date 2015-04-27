a6e6dfdedfbc60f57a91f441167fe304d0e4da8f
EFanZh <efanzh@gmail.com>
2015-04-27 16:41:49 +0800

    LeetCode OJ algorithm problem 33 is done

---
+++ Visual Studio/Misc/LeetCode OJ/033. Search in Rotated Sorted Array/Solution.h
@@ -0,0 +1,52 @@
+#pragma once
+
+class Solution
+{
+    template <class T, class TValue>
+    static int searchHelper(T first, T last, TValue value)
+    {
+        auto it = lower_bound(first, last, value);
+
+        if (it == last || *it != value)
+        {
+            return -1;
+        }
+        else
+        {
+            return it - first;
+        }
+    }
+
+public:
+    int search(vector<int> &nums, int target)
+    {
+        if (nums.empty())
+        {
+            return -1;
+        }
+
+        auto it = lower_bound(nums.cbegin(), nums.cend(), nums.back(), greater<int>());
+
+        if (target < nums.back())
+        {
+            int result = searchHelper(it, nums.cend() - 1, target);
+
+            if (result == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                return (it - nums.cbegin()) + result;
+            }
+        }
+        else if (nums.back() < target)
+        {
+            return searchHelper(nums.cbegin(), it, target);
+        }
+        else
+        {
+            return nums.size() - 1;
+        }
+    }
+};
