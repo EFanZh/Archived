926012a07eef2ea4b0c42a49d3e4998c3dbcbce3
EFanZh <efanzh@gmail.com>
2015-07-22 18:48:05 +0800

    LeetCode OJ algorithm problem 201 is done

--- Visual Studio/Misc/LeetCode OJ/201. Bitwise AND of Numbers Range/Solution.h
+++ Visual Studio/Misc/LeetCode OJ/201. Bitwise AND of Numbers Range/Solution.h
@@ -1,1 +1,29 @@
 #pragma once
+
+class Solution
+{
+    static unsigned int rangeBitwiseAndHelper(unsigned int m, unsigned int n)
+    {
+        unsigned int result = 0;
+        unsigned int offset = n - m;
+
+        for (unsigned int i = 0; (1u << i) <= m; ++i)
+        {
+            unsigned int bit = 1u << i;
+
+            if ((m & bit) &&
+                (offset < bit - (m & (bit - 1))))
+            {
+                result |= bit;
+            }
+        }
+
+        return result;
+    }
+
+public:
+    int rangeBitwiseAnd(int m, int n)
+    {
+        return static_cast<int>(rangeBitwiseAndHelper(m, n));
+    }
+};
