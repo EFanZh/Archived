2f7dd08e258b8be93e79511ba5192c87d5f4a4af
EFanZh <efanzh@gmail.com>
2015-10-23 08:20:21 +0800

    Change Maybe to Nullable

--- Racket/color-theme/color-theme.rkt
+++ Racket/color-theme/color-theme.rkt
@@ -1,17 +1,16 @@
 #lang typed/racket
 
-(define-type (Maybe t) (U Null t))
+(define-type (Nullable t) (U Null t))
 
 (struct Color ([red : Byte]
                [green : Byte]
                [blue : Byte]
                [alpha : Byte]))
 
-(struct Style ([background : (Maybe Color)]
-               [foreground : (Maybe Color)]
-               [isBold : (Maybe Boolean)]
-               [isItalic : (Maybe Boolean)]))
+(struct Style ([background : (Nullable Color)]
+               [foreground : (Nullable Color)]
+               [isBold : (Nullable Boolean)]
+               [isItalic : (Nullable Boolean)]))
 
 (struct Node ([styles : (Listof Style)]
               [children : (Listof Node)]))
-
