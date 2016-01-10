19cf7aa5165baeac9e995dbdd146c4ccf58b50ac
EFanZh <efanzh@gmail.com>
2016-01-10 19:36:31 +0800

    Refactor Generate Font List

--- Visual Studio/Applications/Font Viewer/GenerateFontList/Program.cs
+++ Visual Studio/Applications/Font Viewer/GenerateFontList/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using DirectWriteWrapper;
@@ -7,26 +8,52 @@ namespace GenerateFontList
 {
     internal class Program
     {
+        private static readonly KeyValuePair<string, string>[] Languages =
+        {
+            new KeyValuePair<string, string>("ASCII", @"!""#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"),
+            new KeyValuePair<string, string>("English", "I can eat glass and it doesn’t hurt me."),
+            new KeyValuePair<string, string>("Chinese Simplified", "我能吞下玻璃而不伤身体。"),
+            new KeyValuePair<string, string>("Chinese Traditional", "我能吞下玻璃而不傷身體。"),
+            new KeyValuePair<string, string>("Japanese", "私はガラスを食べられます。それは私を傷つけません。")
+        };
+
+        private static bool CanShowSample(Font font, string sample)
+        {
+            return sample.All(c => font.HasCharacter(c));
+        }
+
+        private static string GenerateLanguageFonts(KeyValuePair<string, string> language, IEnumerable<Font> fonts)
+        {
+            var stringBuilder = new StringBuilder($@"\section{{{language.Key}}}");
+
+            stringBuilder.AppendLine();
+
+            foreach (var fontFamily in fonts.GroupBy(f => f.FontFamily.FamilyNames.GetPreferedString()))
+            {
+                stringBuilder.AppendLine($@"\subsection{{{fontFamily.Key.EscapseToLaTeX()}}}");
+
+                fontFamily.ToLaTeX(stringBuilder, 0, language.Value);
+            }
+
+            return stringBuilder.ToString();
+        }
+
         private static void Main()
         {
             var factory = new Factory(FactoryType.Shared);
 
-            var fontFamilies = (from f in factory.GetSystemFontCollection(true)
-                                where !f.GetFirstMatchingFont(FontWeight.Normal, FontStretch.Normal, FontStyle.Normal).IsSymbolFont
-                                orderby f.FamilyNames.GetPreferedString()
-                                select f).ToArray();
+            var fonts = (from f in factory.GetSystemFontCollection(true)
+                             //where !f.GetFirstMatchingFont(FontWeight.Normal, FontStretch.Normal, FontStyle.Normal).IsSymbolFont
+                         orderby f.FamilyNames.GetPreferedString()
+                         select f).SelectMany(f => f.GetMatchingFonts(FontWeight.Normal, FontStretch.Normal, FontStyle.Normal)).ToArray();
 
-            var stringBuilder = new StringBuilder();
-
-            fontFamilies.First().ToLaTeX(stringBuilder, 0);
-
-            foreach (var fontFamily in fontFamilies.Skip(1))
+            foreach (var language in Languages)
             {
-                stringBuilder.AppendLine();
-                fontFamily.ToLaTeX(stringBuilder, 0);
-            }
+                var languageFonts = fonts.Where(f => CanShowSample(f, language.Value));
+                var result = GenerateLanguageFonts(language, languageFonts);
 
-            File.WriteAllText("E:\\1.txt", stringBuilder.ToString(), Encoding.UTF8);
+                File.WriteAllText($@"E:\\{language.Key.Replace(' ', '-').ToLowerInvariant()}.tex", result, Encoding.UTF8);
+            }
         }
     }
 }
--- Visual Studio/Applications/Font Viewer/GenerateFontList/Utility.cs
+++ Visual Studio/Applications/Font Viewer/GenerateFontList/Utility.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DirectWriteWrapper;
@@ -9,16 +8,7 @@ namespace GenerateFontList
     internal static class Utility
     {
         private static readonly string[] PrederedLanguages = { "en-us" };
-        private static readonly string[] Headers = { "Stretch", "Weight", "Style", "Name", "Language", "Sample" };
-
-        private static readonly KeyValuePair<string, string>[] SampleTexts =
-        {
-             new KeyValuePair<string, string>("ASCII", @"!""#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"),
-             new KeyValuePair<string, string>("Eng", "I can eat glass and it doesn’t hurt me."),
-             new KeyValuePair<string, string>("Chs", "我能吞下玻璃而不伤身体。"),
-             new KeyValuePair<string, string>("Cht", "我能吞下玻璃而不傷身體。"),
-             new KeyValuePair<string, string>("Jap", "私はガラスを食べられます。それは私を傷つけません。")
-        };
+        private static readonly string[] Headers = { "Stretch", "Weight", "Style", "Name", "Sample" };
 
         public static string GetPreferedString(this LocalizedStrings languageSpecificStringDictionary)
         {
@@ -27,7 +17,7 @@ namespace GenerateFontList
             return PrederedLanguages.Any(k => languageSpecificStringDictionary.TryGetValue(k, out result)) ? result : languageSpecificStringDictionary.Values.First();
         }
 
-        private static string EscapseToLaTeX(this string text)
+        public static string EscapseToLaTeX(this string text)
         {
             var sb = new StringBuilder();
 
@@ -76,22 +66,12 @@ namespace GenerateFontList
             return sb.ToString();
         }
 
-        private static IEnumerable<Tuple<Font, KeyValuePair<string, string>>> GetFonts(this FontFamily fontFamily)
-        {
-            var fonts = fontFamily.GetMatchingFonts(FontWeight.Normal, FontStretch.Normal, FontStyle.Normal);
-
-            return (from font in fonts
-                    from sampleText in SampleTexts
-                    where sampleText.Value.All(c => font.HasCharacter(c))
-                    select Tuple.Create(font, sampleText)).ToArray();
-        }
-
-        private static string GetDisplayString(this FontStretch fontStretch)
+        public static string GetDisplayString(this FontStretch fontStretch)
         {
             return $"{(int)fontStretch} ({fontStretch})";
         }
 
-        private static string GetDisplayString(this FontWeight fontWeight)
+        public static string GetDisplayString(this FontWeight fontWeight)
         {
             return (int)fontWeight % 100 == 0 ? $"{(int)fontWeight} ({fontWeight})" : ((int)fontWeight).ToString();
         }
@@ -125,96 +105,80 @@ namespace GenerateFontList
             sb.AppendLine();
         }
 
-        public static void ToLaTeX(this FontFamily fontFamily, StringBuilder stringBuilder, int indent)
+        public static void ToLaTeX(this IEnumerable<Font> fonts, StringBuilder stringBuilder, int indent, string sampleText)
         {
-            stringBuilder.AppendLineWithIndentFormat(indent, @"\section{{{0}}}", fontFamily.FamilyNames.GetPreferedString().EscapseToLaTeX());
-            stringBuilder.AppendLine();
             stringBuilder.AppendLineWithIndentFormat(indent, @"\begin{{tabular}}{{|{0}|}}", string.Join("|", Headers.Select(s => "l")));
             stringBuilder.AppendLineWithIndent(indent + 1, @"\hline");
             stringBuilder.AppendLineWithIndentFormat(indent + 1, @"{0} \\", string.Join(" & ", Headers));
             stringBuilder.AppendLineWithIndent(indent + 1, @"\hline");
 
-            var stretches = from t in fontFamily.GetFonts()
-                            orderby t.Item1.Weight, t.Item1.Stretch
-                            group t by t.Item1.Stretch;
+            var stretches = from t in fonts
+                            orderby t.Weight, t.Stretch
+                            group t by t.Stretch;
 
             foreach (var stretch in stretches)
             {
-                stretch.ToLaTeX(stringBuilder, indent + 1);
+                stretch.ToLaTeX(stringBuilder, indent + 1, sampleText);
                 stringBuilder.AppendLineWithIndent(indent + 1, @"\hline");
             }
             stringBuilder.AppendLineWithIndent(indent, @"\end{tabular}");
         }
 
-        private static void ToLaTeX(this IGrouping<FontStretch, Tuple<Font, KeyValuePair<string, string>>> stretch, StringBuilder stringBuilder, int indent)
+        private static void ToLaTeX(this IGrouping<FontStretch, Font> stretch, StringBuilder stringBuilder, int indent, string sampleText)
         {
             stringBuilder.AppendWithIndentFormat(indent, @"\multirow{{{0}}}{{*}}{{{1}}} & ", stretch.Count(), stretch.Key.GetDisplayString());
 
-            var weights = stretch.GroupBy(t => t.Item1.Weight).ToArray();
+            var weights = stretch.GroupBy(t => t.Weight).ToArray();
 
-            weights.First().ToLaTeX(stringBuilder, indent);
+            weights.First().ToLaTeX(stringBuilder, indent, sampleText);
 
             foreach (var weight in weights.Skip(1))
             {
                 stringBuilder.AppendLineWithIndentFormat(indent, @"\cline{{2-{0}}}", Headers.Length);
                 stringBuilder.AppendWithIndent(indent, "& ");
-                weight.ToLaTeX(stringBuilder, indent);
+                weight.ToLaTeX(stringBuilder, indent, sampleText);
             }
         }
 
-        private static void ToLaTeX(this IGrouping<FontWeight, Tuple<Font, KeyValuePair<string, string>>> weight, StringBuilder stringBuilder, int indent)
+        private static void ToLaTeX(this IGrouping<FontWeight, Font> weight, StringBuilder stringBuilder, int indent, string sampleText)
         {
             stringBuilder.AppendWithIndentFormat(0, @"\multirow{{{0}}}{{*}}{{{1}}} & ", weight.Count(), weight.Key.GetDisplayString());
 
-            var styles = weight.GroupBy(t => t.Item1.Style).ToArray();
+            var styles = weight.GroupBy(t => t.Style).ToArray();
 
-            styles.First().ToLaTeX(stringBuilder, indent);
+            styles.First().ToLaTeX(stringBuilder, indent, sampleText);
 
             foreach (var style in styles.Skip(1))
             {
                 stringBuilder.AppendLineWithIndentFormat(indent, @"\cline{{3-{0}}}", Headers.Length);
                 stringBuilder.AppendWithIndent(indent, "& & ");
-                style.ToLaTeX(stringBuilder, indent);
+                style.ToLaTeX(stringBuilder, indent, sampleText);
             }
         }
 
-        private static void ToLaTeX(this IGrouping<FontStyle, Tuple<Font, KeyValuePair<string, string>>> style, StringBuilder stringBuilder, int indent)
+        private static void ToLaTeX(this IGrouping<FontStyle, Font> style, StringBuilder stringBuilder, int indent, string sampleText)
         {
             stringBuilder.AppendWithIndentFormat(0, @"\multirow{{{0}}}{{*}}{{{1}}} & ", style.Count(), style.Key.ToString());
 
-            var fonts = style.GroupBy(t => t.Item1).ToArray();
+            var fonts = style.GroupBy(t => t).ToArray();
 
-            fonts.First().ToLaTeX(stringBuilder, indent);
+            fonts.First().ToLaTeX(stringBuilder, sampleText);
 
             foreach (var font in fonts.Skip(1))
             {
                 stringBuilder.AppendLineWithIndentFormat(indent, @"\cline{{4-{0}}}", Headers.Length);
                 stringBuilder.AppendWithIndent(indent, "& & & ");
-                font.ToLaTeX(stringBuilder, indent);
+                font.ToLaTeX(stringBuilder, sampleText);
             }
         }
 
-        private static void ToLaTeX(this IGrouping<Font, Tuple<Font, KeyValuePair<string, string>>> font, StringBuilder stringBuilder, int indent)
-        {
-            stringBuilder.AppendWithIndentFormat(0, @"\multirow{{{0}}}{{*}}{{{1}}} & ", font.Count(), font.Key.FaceNames.GetPreferedString());
-
-            font.First().ToLaTeX(stringBuilder);
-
-            foreach (var sampleText in font.Skip(1))
-            {
-                stringBuilder.AppendLineWithIndentFormat(indent, @"\cline{{5-{0}}}", Headers.Length);
-                stringBuilder.AppendWithIndent(indent, "& & & & ");
-                sampleText.ToLaTeX(stringBuilder);
-            }
-        }
-
-        private static void ToLaTeX(this Tuple<Font, KeyValuePair<string, string>> sampleText, StringBuilder stringBuilder)
+        private static void ToLaTeX(this IGrouping<Font, Font> font, StringBuilder stringBuilder, string sampleText)
         {
             stringBuilder.AppendLineWithIndentFormat(0,
-                @"{0} & \sample{{{1}}}{{{2}}} \\",
-                sampleText.Item2.Key,
-                sampleText.Item1.GetInformationalStrings(InformationalStringId.PostScriptName).GetPreferedString(),
-                sampleText.Item2.Value.EscapseToLaTeX());
+                                                     @"{0} & \sample{{{1}}}{{{2}}} \\",
+                                                     font.Key.FaceNames.GetPreferedString(),
+                                                     font.Key.GetInformationalStrings(InformationalStringId.PostScriptName).GetPreferedString(),
+                                                     sampleText.EscapseToLaTeX());
         }
     }
 }
