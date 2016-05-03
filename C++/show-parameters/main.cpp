d8944e13fbbd84dbbe4e85d20d77b36621a41cd1
EFanZh <efanzh@gmail.com>
2016-05-03 12:42:37 +0800

    Add a tool that displays the command line parameters

---
+++ C++/show-parameters/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+inline void PrintQuotedCharacter(char ch)
+{
+    switch (ch)
+    {
+        case '\"':
+            cout << '\\' << '"';
+            break;
+
+        case '\\':
+            cout << '\\' << '\\';
+            break;
+
+        case '\a':
+            cout << '\\' << 'a';
+            break;
+
+        case '\b':
+            cout << '\\' << 'b';
+            break;
+
+        case '\f':
+            cout << '\\' << 'f';
+            break;
+
+        case '\n':
+            cout << '\\' << 'n';
+            break;
+
+        case '\r':
+            cout << '\\' << 'r';
+            break;
+
+        case '\t':
+            cout << '\\' << 't';
+            break;
+
+        case '\v':
+            cout << '\\' << 'v';
+            break;
+
+        default:
+            cout << ch;
+            break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    for (auto i = 0; i < argc; ++i)
+    {
+        cout << '"';
+
+        for (auto p = argv[i]; *p != '\0'; ++p)
+        {
+            PrintQuotedCharacter(*p);
+        }
+
+        cout << '"' << '\n';
+    }
+}
