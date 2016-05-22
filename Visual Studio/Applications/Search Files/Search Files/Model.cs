8e187fee7c204ce7c1cf29f7f1ddc3814a77831e
EFanZh <efanzh@gmail.com>
2016-05-22 09:54:05 +0800

    Update Search Files

--- Visual Studio/Applications/Search Files/Search Files/Model.cs
+++ Visual Studio/Applications/Search Files/Search Files/Model.cs
@@ -67,10 +67,7 @@ namespace SearchFiles
 
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
