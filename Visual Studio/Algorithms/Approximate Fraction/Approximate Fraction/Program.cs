a81bb0fbce079d146fc77a4e74abed6a3900c7a0
EFanZh <efanzh@gmail.com>
2016-10-11 21:34:58 +0800

    Update platform version

--- Visual Studio/Algorithms/Approximate Fraction/Approximate Fraction/Program.cs
+++ Visual Studio/Algorithms/Approximate Fraction/Approximate Fraction/Program.cs
@@ -4,10 +4,12 @@ namespace ApproximateFraction
 {
     internal class Program
     {
+        private const decimal pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036m;
+
         private static void Main(string[] args)
         {
             int a, b;
-            GetApproximateFraction(1.4142135623730950488016887242097m, 1000000, out a, out b);
+            GetApproximateFraction((decimal)Math.Sqrt(3), int.MaxValue, out a, out b);
         }
 
         private static void GetApproximateFraction(decimal number, int max, out int numerator, out int denominator)
@@ -17,7 +19,11 @@ namespace ApproximateFraction
 
             for (int i = 0; i < max; i++)
             {
-                Console.Title = string.Format("{0}", i);
+                if (i % 1000 == 0)
+                {
+                    Console.Title = string.Format("{0}", i);
+                }
+
                 for (int j = 0; j <= i; j++)
                 {
                     a = j + 1;
