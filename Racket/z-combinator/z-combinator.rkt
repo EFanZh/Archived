ed8784eb3521fad4321136763f7e591e8d4cb489
EFanZh <efanzh@gmail.com>
2018-06-06 22:38:26 +0800

    Fix function names

--- Racket/z-combinator/z-combinator.rkt
+++ Racket/z-combinator/z-combinator.rkt
@@ -2,91 +2,99 @@
 
 (require rackunit)
 
-(define fib1 (λ (n)
+(define fact1
+  (λ (n)
+    (if (zero? n)
+        1
+        (* (fact1 (sub1 n)) n))))
+
+(define fact2
+  (let ([fact-helper (λ (self)
+                       (λ (n)
+                         (if (zero? n)
+                             1
+                             (* ((self self) (sub1 n)) n))))])
+    (λ (n)
+      ((fact-helper fact-helper) n))))
+
+(define fact3
+  (let ([fact-helper (λ (self)
+                       (λ (n)
+                         (if (zero? n)
+                             1
+                             (* ((self self) (sub1 n)) n))))])
+    (fact-helper fact-helper)))
+
+(define fact4
+  ((λ (self)
+     (λ (n)
+       (if (zero? n)
+           1
+           (* ((self self) (sub1 n)) n))))
+   (λ (self)
+     (λ (n)
+       (if (zero? n)
+           1
+           (* ((self self) (sub1 n)) n))))))
+
+(define fact5
+  (let ([fact-helper (λ (self)
+                       (λ (n)
+                         (if (zero? n)
+                             1
+                             (* ((self self) (sub1 n)) n))))])
+    ((λ (self)
+       (fact-helper self))
+     (λ (self)
+       (fact-helper self)))))
+
+(define fact6
+  (let ([not-y (λ (f)
+                 ((λ (self)
+                    (f self))
+                  (λ (self)
+                    (f self))))])
+    (not-y (λ (self)
+             (λ (n)
                (if (zero? n)
                    1
-                   (* (fib1 (sub1 n)) n))))
+                   (* ((self self) (sub1 n)) n)))))))
 
-(define fib2 (let ([fib-helper (λ (self)
-                                 (λ (n)
-                                   (if (zero? n)
-                                       1
-                                       (* ((self self) (sub1 n)) n))))])
-               (λ (n)
-                 ((fib-helper fib-helper) n))))
+(define fact7
+  (let ([not-y (λ (f)
+                 ((λ (self)
+                    (f (λ (x)
+                         (self x))))
+                  (λ (self)
+                    (f (λ (x)
+                         (self x))))))])
+    (not-y (λ (self)
+             (λ (n)
+               (if (zero? n)
+                   1
+                   (* ((self self) (sub1 n)) n)))))))
 
-(define fib3 (let ([fib-helper (λ (self)
-                                 (λ (n)
-                                   (if (zero? n)
-                                       1
-                                       (* ((self self) (sub1 n)) n))))])
-               (fib-helper fib-helper)))
-
-(define fib4 ((λ (self)
-                (λ (n)
-                  (if (zero? n)
-                      1
-                      (* ((self self) (sub1 n)) n))))
+(define fact8
+  (let ([z (λ (f)
+             ((λ (self)
+                (f (λ (x)
+                     ((self self) x))))
               (λ (self)
-                (λ (n)
-                  (if (zero? n)
-                      1
-                      (* ((self self) (sub1 n)) n))))))
-
-(define fib5 (let ([fib-helper (λ (self)
-                                 (λ (n)
-                                   (if (zero? n)
-                                       1
-                                       (* ((self self) (sub1 n)) n))))])
-               ((λ (self)
-                  (fib-helper self))
-                (λ (self)
-                  (fib-helper self)))))
-
-(define fib6 (let ([not-y (λ (f)
-                            ((λ (self)
-                               (f self))
-                             (λ (self)
-                               (f self))))])
-               (not-y (λ (self)
-                        (λ (n)
-                          (if (zero? n)
-                              1
-                              (* ((self self) (sub1 n)) n)))))))
-
-(define fib7 (let ([not-y (λ (f)
-                            ((λ (self)
-                               (f (λ (x)
-                                    (self x))))
-                             (λ (self)
-                               (f (λ (x)
-                                    (self x))))))])
-               (not-y (λ (self)
-                        (λ (n)
-                          (if (zero? n)
-                              1
-                              (* ((self self) (sub1 n)) n)))))))
-
-(define fib8 (let ([z (λ (f)
-                        ((λ (self)
-                           (f (λ (x)
-                                ((self self) x))))
-                         (λ (self)
-                           (f (λ (x)
-                                ((self self) x))))))])
-               (z (λ (f)
-                    (λ (n)
-                      (if (zero? n)
-                          1
-                          (* (f (sub1 n)) n)))))))
+                (f (λ (x)
+                     ((self self) x))))))])
+    (z (λ (f)
+         (λ (n)
+           (if (zero? n)
+               1
+               (* (f (sub1 n)) n)))))))
 
 ;; Tests.
 
-(for ([fib (list fib1 fib2 fib3 fib4 fib5 fib6 fib7 fib8)])
-  (check-eq? (fib 0) 1)
-  (check-eq? (fib 1) 1)
-  (check-eq? (fib 2) 2)
-  (check-eq? (fib 3) 6)
-  (check-eq? (fib 4) 24)
-  (check-eq? (fib 5) 120)
-  (check-eq? (fib 6) 720))
+(for ([fact (list fact1 fact2 fact3 fact4 fact5 fact6 fact7 fact8)])
+  (check-eq? (fact 0) 1)
+  (check-eq? (fact 1) 1)
+  (check-eq? (fact 2) 2)
+  (check-eq? (fact 3) 6)
+  (check-eq? (fact 4) 24)
+  (check-eq? (fact 5) 120)
+  (check-eq? (fact 6) 720))
